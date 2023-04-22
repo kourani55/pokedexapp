6f2ba0dbe2e8ca0bc27428b8a81da6a7ec782f79
kourani55 <fakouran@umich.edu>
2023-04-22 17:33:01 -0400

    Update InfoFragment.kt

--- app/src/main/java/com/example/pokedexapp/ui/main/InfoFragment.kt
+++ app/src/main/java/com/example/pokedexapp/ui/main/InfoFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pokedexapp.R
+import com.example.pokedexapp.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
 
+    private lateinit var binding : FragmentInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,11 @@ class InfoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 
